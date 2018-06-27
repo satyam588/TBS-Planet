fba2355cc23545d5376141df524f56019beb08bf
satyam588 <sundaramsatyam12@gmail.com>
2018-06-27 20:44:07 +0530

    TBS Planet Landing Page / Homepage

---
+++ TBS Planet/index.php
@@ -0,0 +1,255 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Indian Comics| TBS Planet </title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+       
+       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+        <!-- Latest compiled JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="index.css">
+        
+        
+    </head>
+    
+    <body>
+        <nav class="navbar navbar-default navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>                        
+              </button>
+                <a class="navbar-brand" href="#"><img class="logo" src="images/logo_dark.png"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="main-nav">
+              <ul class="nav navbar-nav navbar-right">
+                  <li><a href="#" class="btn btn-visit">Visit Store</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        
+        <div id="Carousel" class="carousel slide" data-ride="carousel">
+            
+            <ol class="carousel-indicators">
+              <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+              <li data-target="#Carousel" data-slide-to="1"></li>
+              <li data-target="#Carousel" data-slide-to="2"></li>
+              <li data-target="#Carousel" data-slide-to="3"></li>
+              <li data-target="#Carousel" data-slide-to="4"></li>
+              <li data-target="#Carousel" data-slide-to="5"></li>
+              <li data-target="#Carousel" data-slide-to="6"></li>
+              <li data-target="#Carousel" data-slide-to="7"></li>
+              <li data-target="#Carousel" data-slide-to="8"></li>
+            </ol>
+
+            
+            <div class="carousel-inner">
+              <div class="item active">
+                  <img src="images/offer.jpg" alt="Thor Comics">
+              </div>
+
+              <div class="item">
+                  <img src="images/avnen.jpg" alt="Avengers Comics">
+              </div>
+
+              <div class="item">
+                  <img src="images/batvsup.jpg" alt="Batman Vs Superman">
+              </div>
+                
+              <div class="item">
+                  <img src="images/captan.jpg" alt="Captain America">
+              </div>
+                
+              <div class="item">
+                  <img src="images/daredevil.jpg" alt="DareDevil">
+              </div>
+                
+              <div class="item">
+                  <img src="images/deadpool.jpg" alt="Deadpool">
+              </div>
+                
+              <div class="item">
+                  <img src="images/iron.jpg" alt="Iron Man">
+              </div>
+                
+              <div class="item">
+                  <img src="images/justi_li.jpg" alt="Justice League">
+              </div>
+                
+              <div class="item">
+                  <img src="images/thor.jpg" alt="Thor">
+              </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#Carousel" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#Carousel" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>
+        
+        <div class="offer-wrapper">
+            
+                <div class="container">
+                    <div class="row">
+                        <h1 class="offer-heading" style="margin-top: 50px;">Love Comics?</h1>
+                        <hr style="width: 100px;border: 0.5px solid #000;">
+                        <div class="col-xs-4">
+                            <img class="offer-logo" src="images/offer_logo.png">
+                        </div>
+
+                        <div class="col-xs-8">
+                            <p class="offer-content">Wouldn't it be super cool to read them daily?</p>
+                            <p class="offer-content">Oh yes! New content every day for 365 days a year.</p>
+                        
+                            <div class="offer-btn" align="right">
+                                <button class="btn btn-default" style="margin-top: 50px;">START YOUR 30 DAY FREE TRAIL</button>
+                                <br><br>
+                                <button class="btn btn-default "><strong>SUBSCRIBE FOR A YEAR AT ₹300</strong></button>
+                            </div>
+                        
+                        </div>
+                    </div>
+                </div>
+           
+        </div>
+        
+        <div class="subscription-method">
+            <div class="container">
+                <div class="row">
+                    <h1 class="offer-heading" style="margin-top: 50px;">Discover what all the buzz is about!</h1>
+                    <hr style="width: 100px;border: 0.5px solid #000;">
+                    <div class="col-xs-9">
+                        <p class="sbs-content">
+                            Award Winning Comics. Best Stories. Best Graphics. 
+                            All at your fingertips! And that's not all. You get 
+                            chances to become a character in the comic, you also get 
+                            opportunities to be an author or an artist in certain projects.
+                            And many more such personalized and exciting stuff.
+                        </p>
+                        
+                        <p class="sbs-content">
+                            For just 300 Rs! So basically you get to 
+                            read new comics at less than 1 Re. a day! 
+                            Isn't that even more economical than the good 
+                            old library days, where you had to get comics 
+                            issued for a week and then come home and read?
+                        </p>
+                        
+                        <p class="sbs-content">
+                            So what are you waiting for? Go 
+                            ahead and subscribe! Or if you would 
+                            want to try it out, enjoy our free 30 days 
+                            trial and then decide if TBS Planet is for you or not!
+                        </p>
+                    </div>
+                    
+                    <div class="col-xs-3">
+                        <img class="sbs-image" src="images/sbs.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="faq-section">
+            <div class="container">
+                <h1 style="text-align: center;margin-top: 50px;font-family: nunito-bold;">Have Any Questions?</h1>
+                <p style="color: #fff;text-align: center;font-size: 15px;">Refer Our FAQs.</p>
+                
+                <div align="center" class="jumbotron" ><a>Do I get to choose my favorite genres?</a></div>
+                
+                <div align="center" class="jumbotron" ><a>What about languages? I want to read in my native language. Is that an option?</a></div>
+                
+                <div align="center" class="jumbotron" ><a>I want to gift the subscription to a friend/ family member. Is it possible?</a></div>
+                
+                <div align="center" class="jumbotron" ><a>Where do I get to read the comics?</a></div>
+                
+                <div align="center" class="jumbotron" ><a>I am an artist/ writer and I want to contribute too. Is that possible?</a></div>
+                
+                <div align="center" class="jumbotron"><a>What if I decide this subscription is not interesting for me?</a></div>
+                
+                <div align="center" class="jumbotron"><a>What if I still have a question?</a></div>
+            </div>
+        </div>
+        
+        <div class="we-are">
+            <div class="container">
+                <div class="thumbnail col-xs-4" style="border-left: 0;">
+                    <i class="fab fa-whatsapp"></i>
+                    <h3 style="margin: 30px 0;">Delivered directly to your WhatsApp</h3>
+                </div>
+                
+                <div class="thumbnail col-xs-4">
+                    <i class="fas fa-book-open"></i>
+                    <h3 style="margin: 30px 0;">Full entertainment</h3>
+                </div>
+                
+                <div class="thumbnail col-xs-4">
+                    <i class="fas fa-users"></i>
+                    <h3 style="margin: 30px 0;">Family friendly</h3>
+                </div>
+            </div>
+        </div>
+        
+        
+        <div class="explore-now">
+            <div class="container">
+                <h1 class="explore-content">
+                    Stop<br/>
+                    Thinking.<br/>
+                    Start<br/>
+                    Exploring.<br/>
+                    
+                    <button class="btn btn-default" style="margin-top: 50px;background-color: rgba(0,0,0,0.5);color: #fff;font-size: 12px;padding: 15px 60px;border-radius: 30px;">SUBSCRIBE DIGITAL COMICS FOR A YEAR</button>
+                </h1>
+            </div>
+        </div>
+        
+        <div class="social">
+            <h1 style="margin: 0;padding-top: 50px;text-align: center;font-family: nunito-bold;">We are social!</h1>
+            <hr style="width: 100px;border: 0.5px solid #000;">
+            
+            <div align="center">
+                <i class="fab fa-facebook-f"></i>
+                <i class="fab fa-instagram"></i>
+            </div>
+        </div>
+        
+        <footer>
+            <div class="container">
+                <p style="text-align: center">&copy; TBS Planet Comics 2018. All Rights Reserved.</p>
+                
+                <div align="center">
+                <a href="#" >Home</a>
+                <a href="#" >Terms</a>
+                <a href="#" >Contact</a>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
+
